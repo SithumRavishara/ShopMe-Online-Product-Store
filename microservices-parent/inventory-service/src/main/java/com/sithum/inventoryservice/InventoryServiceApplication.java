1a0e2491faea13010f10b5029e099714d94cf308
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2023-03-10 19:59:28 +0530

    initial commit

--- microservices-parent/inventory-service/src/main/java/com/sithum/inventoryservice/InventoryServiceApplication.java
+++ microservices-parent/inventory-service/src/main/java/com/sithum/inventoryservice/InventoryServiceApplication.java
@@ -1,5 +1,8 @@
 package com.sithum.inventoryservice;
 
+import com.sithum.inventoryservice.model.Inventory;
+import com.sithum.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +13,19 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iPhone_14");
+			inventory.setQuantity(20);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iPhone_12");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
 }
